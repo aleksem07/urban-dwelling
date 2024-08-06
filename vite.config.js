c20c047f0de920f051878c22f8cd2aa02e18b00d
aleksem07 <aleksem07@gmail.com>
2024-08-06 12:40:23 +0300

    feat: add sprites

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import vituum from "vituum";
 import pug from "@vituum/vite-plugin-pug";
 import postcss from "@vituum/vite-plugin-postcss";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import ViteSvgSpriteWrapper from "vite-svg-sprite-wrapper";
 
 export default {
   publicDir: "public",
@@ -22,5 +24,9 @@ export default {
       root: "./src",
     }),
     postcss(),
+    ViteSvgSpriteWrapper({
+      icons: "public/icons/*.svg",
+      outputDir: "./public",
+    }),
   ],
 };
