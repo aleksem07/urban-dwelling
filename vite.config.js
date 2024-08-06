48346e9a0d78a4234113c19f461dff25ad76939b
aleksem07 <aleksem07@gmail.com>
2024-08-06 13:46:34 +0300

    fix: fix nav panel desktop-xl

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import pug from "@vituum/vite-plugin-pug";
 import postcss from "@vituum/vite-plugin-postcss";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import ViteSvgSpriteWrapper from "vite-svg-sprite-wrapper";
+import autoprefixer from "autoprefixer";
 
 export default {
   publicDir: "public",
@@ -23,7 +24,9 @@ export default {
     pug({
       root: "./src",
     }),
-    postcss(),
+    postcss({
+      plugins: [autoprefixer()],
+    }),
     ViteSvgSpriteWrapper({
       icons: "public/icons/*.svg",
       outputDir: "./public",
